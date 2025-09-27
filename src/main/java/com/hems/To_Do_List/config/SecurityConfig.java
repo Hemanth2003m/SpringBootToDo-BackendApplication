54fcd822a5f8654c7dada90dc507acbb8553e1ee
Chitikela Hemanth <22p31a05f1@acet.ac.in>
2025-09-27 12:28:39 +0530

    Clarify CORS allowedOrigins comment
    Updated CORS configuration comment for clarity.

--- src/main/java/com/hems/To_Do_List/config/SecurityConfig.java
+++ src/main/java/com/hems/To_Do_List/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React dev server
+                        .allowedOrigins("http://localhost:5173") // After the frontend deployment the ip is enough dont need port
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
